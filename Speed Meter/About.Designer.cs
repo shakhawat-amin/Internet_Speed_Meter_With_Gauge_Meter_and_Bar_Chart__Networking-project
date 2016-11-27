bcb91ae68245e098b10b4bc2924363e0ed22c943
shakhawat-amin <s.shakhawat.amin@gmail.com>
2016-11-28 03:22:12 +0600

    Complete Project

---
+++ Speed Meter/About.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Speed_Meter
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Developed By";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(62, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Dept. of CSE, SUST";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Shakhawat Amin";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(46, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 19);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Dhruba Chakraborty";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(80, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "2012331009";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(80, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "2012331070";
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(254, 212);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "About";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.About_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Speed Meter/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using Add_And_Remove_Programme_To_Or_From_Startup;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace Speed_Meter
+{
+    public partial class Form1 : Form
+    {
+        private NetworkInterface[] nicArr;
+        private NetworkInterface activeConnection;
+        string bytesent = "0";
+        string byterecieved = "0";
+        private bool isMaximized = false;
+        private bool close = true;
+        int x = 0;
+        double [] chartUploadSpeed= new double [20];
+        double[] chartDownloadSpeed = new double [20];
+
+        //string startupKey = "speedmeter";
+
+        private long[] byteRecieved;
+        private long[] byteSent;
+        private bool[] connection;
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        public Form1()
+        {
+            this.Location = new Point(400, 380);
+            InitializeComponent();
+            init();
+            timer1.Start();
+
+            //Startup.AddProgrammeToStartUp(startupKey);
+           
+
+        }
+
+        private void init()
+        {
+            nicArr = NetworkInterface.GetAllNetworkInterfaces();
+            byteRecieved = new long[nicArr.Length];
+            byteSent = new long[nicArr.Length];
+            connection = new bool[nicArr.Length];
+
+            for(int i = 0; i < nicArr.Length; i++)
+            {
+                connection[i] = false;
+                byteSent[i] = 0;
+                byteRecieved[i] = 0;
+            }
+            for (int i = 0; i < 20; i++)
+                chartDownloadSpeed[i] = chartUploadSpeed[i] = 0;
+            
+            
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoom(0.0, 20.0);
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoom(0.0, 20.0);
+            
+            chart1.ChartAreas[0].CursorX.IsUserEnabled = true;
+            chart1.ChartAreas[0].CursorY.IsUserEnabled = true;
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+            //chart1.Series.Add("download");
+            //chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+
+        }
+        private void checkAvailableConnection()
+        {
+            bool flag = false;
+            int pos = 0;
+            IPv4InterfaceStatistics interfaceStats;
+            
+            for(int i = 0; i < nicArr.Length; i++)
+            {
+                interfaceStats = nicArr[i].GetIPv4Statistics();
+                if(interfaceStats.BytesReceived != byteRecieved[i] || interfaceStats.BytesSent != byteSent[i])
+                {
+                    activeConnection = nicArr[i];
+                    flag = true;
+                    pos = i;
+                   
+                }
+
+              
+
+            }
+            
+            int byteSentPerSec;
+            int byteRecievedPerSec;
+
+            if(flag){
+                interfaceStats = nicArr[pos].GetIPv4Statistics();
+                byteSentPerSec = (int)(interfaceStats.BytesSent - (double)byteSent[pos]) / 1024;
+                byteRecievedPerSec = (int)(interfaceStats.BytesReceived - (double)byteRecieved[pos]) / 1024;
+            }
+
+            else
+            {
+                byteRecievedPerSec = 0;
+                byteSentPerSec = 0;
+            }
+
+            int modForChart = x % 20;
+
+
+            if (byteSentPerSec > 1000)
+                byteSentPerSec = 1000;
+            if (byteRecievedPerSec > 1000)
+                byteRecievedPerSec = 1000;
+            chartDownloadSpeed[modForChart] = byteSentPerSec;
+            chartUploadSpeed[modForChart] = byteRecievedPerSec;
+            //for (int i = 0; i < chart1.Series[0].Points.Count; i++)
+            //{
+            //    chart1.Series[0].Points.RemoveAt(i);
+            //}
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            int j = 0;
+            for (int i = modForChart + 1; i < 20; i++, j++)
+            {
+                System.Console.WriteLine(modForChart);
+                chart1.Series[1].Points.AddXY((double)j, (double)chartUploadSpeed[i]);
+                //chart1.Series[0].Points[j].Color = System.Drawing.Color.Red;
+                chart1.Series[0].Points.AddXY((double)j, (double) chartDownloadSpeed[i]);
+            }
+            for (int i = 0; i <= modForChart; i++,j++)
+            {
+                chart1.Series[1].Points.AddXY((double)j, (double) chartUploadSpeed[i]);
+                chart1.Series[0].Points.AddXY((double)j, (double) chartDownloadSpeed[i]);
+            }
+           // chart1.Series[0].Points.AddXY(0.0, 00.0);
+            
+           // chart1.Series[0].Points.AddXY(1.0, 10.0);
+           // chart1.Series[0].Points.AddXY(2.0, 5.0);
+           //// chart1.Series[0].Points.AddXY(3.0, 300.0);
+           // chart1.Series[0].Points.AddXY(3.0, 20.0);
+
+            lblUploadSpeed.Text = byteSentPerSec + " kb/s";
+            lblAgueUp.Text = byteSentPerSec + " kb/s";
+
+            lbldownload.Text = byteRecievedPerSec + " kb/s";
+            lblAgueDown.Text = byteRecievedPerSec + " kb/s";
+
+
+            aGaugeDown.Value = byteRecievedPerSec;
+            aGaugeUp.Value = byteSentPerSec;
+
+            if (close && x > 0)
+            {
+                this.Size = new Size(339, 36);
+                close = false;
+            }
+
+            for(int i = 0; i < nicArr.Length; i++)
+            {
+                interfaceStats = nicArr[i].GetIPv4Statistics();
+
+                byteSent[i] = interfaceStats.BytesSent;
+                byteRecieved[i] = interfaceStats.BytesReceived;
+            }
+
+            
+
+        }
+
+       
+
+
+        private void initialize()
+        {
+           
+            nicArr = NetworkInterface.GetAllNetworkInterfaces();
+            NetworkInterface n = nicArr[3];
+            for(int i = 0; i < nicArr.Length; i++)
+            {
+                System.Console.WriteLine(nicArr[i].Name);
+                if (nicArr[i].Name == "Wi-Fi")
+                {
+                    n = nicArr[i];
+                    break;
+                }
+                
+            }
+            
+            IPv4InterfaceStatistics interfaceStats = n.GetIPv4Statistics();
+
+
+            int bytesSentSpeed = (int)(interfaceStats.BytesSent - double.Parse(bytesent)) / 1024;
+            int bytesReceivedSpeed = (int)(interfaceStats.BytesReceived - double.Parse(byterecieved)) / 1024;
+
+            bytesent = interfaceStats.BytesSent.ToString();
+            byterecieved = interfaceStats.BytesReceived.ToString();
+
+            lblUploadSpeed.Text = bytesSentSpeed.ToString() + " kb/s";
+            lblAgueUp.Text = bytesSentSpeed.ToString() + " kb/s";
+
+            lbldownload.Text = bytesReceivedSpeed.ToString() + " kb/s";
+            lblAgueDown.Text = bytesReceivedSpeed.ToString() + " kb/s";
+
+            aGaugeDown.Value = bytesReceivedSpeed;
+            aGaugeUp.Value = bytesSentSpeed;
+
+            if (close && x > 0)
+            {
+                this.Size = new Size(339, 36);
+                close = false;
+            }
+
+
+        }
+
+                                                                                                    
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x++;
+            System.Console.WriteLine(x);
+           // initialize();
+
+            checkAvailableConnection();
+            
+        }
+
+
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (isMaximized == true)
+            {
+                this.Size = new Size(339, 36);
+                button1.Text = "Max";
+                isMaximized = false;
+                //this.Location = new Point(this.Location.X + 120, this.Location.Y + 300);
+            }
+            else if (isMaximized == false)
+            {
+                this.Size = new Size(864, 352);
+                button1.Text = "Min";
+                isMaximized = true;
+               // this.Location = new Point(this.Location.X - 120, this.Location.Y - 300);
+            }
+
+        }
+
+         private void FromMoveWithouse(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void Form1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            About obj = new About();
+            obj.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAgueUp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblUploadSpeed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAgueDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
